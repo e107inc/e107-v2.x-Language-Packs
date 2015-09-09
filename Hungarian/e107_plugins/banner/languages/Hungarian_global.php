dd1fbfd2b8630283a6a87162d612d759d56c2f93
Lóna Lore <lonalore@freemail.hu>
2015-09-09 12:11:44 +0200

    reviewed translations for banner plugin

--- Hungarian/e107_plugins/banner/languages/Hungarian_global.php
+++ Hungarian/e107_plugins/banner/languages/Hungarian_global.php
@@ -5,4 +5,4 @@
 // https://github.com/e107inc/multilan
 
 define("LAN_PLUGIN_BANNER_NAME", "Bannerek");
-define("LAN_PLUGIN_BANNER_DESCRIPTION", "Reklám bannerek hozzá az e107 honlapja");
+define("LAN_PLUGIN_BANNER_DESCRIPTION", "Reklám bannerek kihelyezése az e107 weblapra");
