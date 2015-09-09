895b86e0238c92d733643001160253dae121baa5
Lóna Lore <lonalore@freemail.hu>
2015-09-09 16:33:43 +0200

    reviewed translations for featurebox plugin

--- Hungarian/e107_plugins/featurebox/languages/Hungarian_global.php
+++ Hungarian/e107_plugins/featurebox/languages/Hungarian_global.php
@@ -4,11 +4,12 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("LAN_PLUGIN_FEATUREBOX_NAME", "Funkcióihoz");
-define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Egy élénk környéken, Hírek-cikkek és egyéb tartalmak, azt szeretné, hogy a szolgáltatás a lap tetején jelenik meg.");
-define("LAN_PLUGIN_FEATUREBOX_BATCH", "Featurebox-elem létrehozása");
-define("FBLAN_INSTALL_01", "Alapértelmezett kategória tábla adatok hozzáadása.");
-define("FBLAN_INSTALL_02", "Alapértelmezett tábla adatok hozzáadása.");
-define("FBLAN_INSTALL_03", "Nem hozzárendelt");
-define("FBLAN_INSTALL_04", "Körhinta");
-define("FBLAN_INSTALL_05", "Lapok");
+define("LAN_PLUGIN_FEATUREBOX_NAME", "Feature Box");
+define("LAN_PLUGIN_FEATUREBOX_DESCRIPTION", "Hírek és egyéb elemek megjelenítése az oldal tetején egy animált területen.");
+define("LAN_PLUGIN_FEATUREBOX_BATCH", "Elem hozzáadása");
+
+define("FBLAN_INSTALL_01", "Alapértelmezett kategóriák hozzáadása.");
+define("FBLAN_INSTALL_02", "Alapértelmezett értékek hozzáadása.");
+define("FBLAN_INSTALL_03", "Nincs hozzárendelve");
+define("FBLAN_INSTALL_04", "Carousel");
+define("FBLAN_INSTALL_05", "Fülek");
