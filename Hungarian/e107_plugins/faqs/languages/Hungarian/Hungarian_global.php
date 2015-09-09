641452d1975479cf59ea1ddc3fd688c68b8507e1
Lóna Lore <lonalore@freemail.hu>
2015-09-09 16:06:04 +0200

    reviewed translations for faqs plugin

--- Hungarian/e107_plugins/faqs/languages/Hungarian/Hungarian_global.php
+++ Hungarian/e107_plugins/faqs/languages/Hungarian/Hungarian_global.php
@@ -5,7 +5,7 @@
 // https://github.com/e107inc/multilan
 
 define("LAN_PLUGIN_FAQS_NAME", "Gyakran ismételt kérdések");
-define("LAN_PLUGIN_FAQS_DESCRIPTION", "Egy egyszerű plugin hozzá a gyakran ismételt kérdések a honlapon.");
-define("LAN_PLUGIN_FAQS_FUNCTIONNAME", "GYIK-kategóriák");
+define("LAN_PLUGIN_FAQS_DESCRIPTION", "Egyszerű GYIK plugin.");
+define("LAN_PLUGIN_FAQS_FUNCTIONNAME", "GYIK kategóriák");
 define("LAN_PLUGIN_FAQS_FRONT_NAME", "GYIK (gyakran ismételt kérdések)");
-define("LAN_PLUGIN_FAQS_SEARCH", "Keresési FAQ");
+define("LAN_PLUGIN_FAQS_SEARCH", "GYIK Kereső");
