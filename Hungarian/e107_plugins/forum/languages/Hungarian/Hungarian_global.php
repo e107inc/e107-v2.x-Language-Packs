91b258a5bfd4289134e95a4610587aac55b8806f
Lóna Lore <lonalore@freemail.hu>
2015-09-10 16:00:45 +0200

    reviewed translations for forum plugin

--- Hungarian/e107_plugins/forum/languages/Hungarian/Hungarian_global.php
+++ Hungarian/e107_plugins/forum/languages/Hungarian/Hungarian_global.php
@@ -5,10 +5,12 @@
 // https://github.com/e107inc/multilan
 
 define("LAN_PLUGIN_FORUM_NAME", "Fórum");
-define("LAN_PLUGIN_FORUM_DESC", "Ez a plugin egy teljes értékű fórum rendszer.");
-define("LAN_PLUGIN_FORUM_POSTS", "Fórum hozzászólás");
-define("LAN_PLUGIN_FORUM_ALLFORUMS", "Minden fórumok");
-define("FORUM_LAN_URL_DEFAULT_LABEL", "Alapértelmezett fórum URL-címek");
-define("FORUM_LAN_URL_DEFAULT_DESCR", "URL típusú'kap' a nem egységes belépési pontot. Példák:<br>http://yoursite.com/e107_plugins/Forum/forum.php (index fórum)<br>http://yoursite.com/e107_plugins/Forum/forum_viewtopic.php?id=2 (a hozzászóláslánc-nézetet)");
-define("FORUM_LAN_URL_REWRITE_LABEL", "SEF fórum URL-eket (fejlesztés alatt)");
+define("LAN_PLUGIN_FORUM_DESC", "Ez a plugin egy teljes értékű fórum rendszert kínál.");
+define("LAN_PLUGIN_FORUM_POSTS", "Fórum bejegyzések");
+define("LAN_PLUGIN_FORUM_ALLFORUMS", "Összes fórum");
+
+define("FORUM_LAN_URL_DEFAULT_LABEL", "Alapértelmezett fórum URL címek");
+define("FORUM_LAN_URL_DEFAULT_DESCR", "\"GET\" típusú URL-ek. Példák:<br>http://yoursite.com/e107_plugins/Forum/forum.php (index fórum)<br>http://yoursite.com/e107_plugins/Forum/forum_viewtopic.php?id=2 (a hozzászóláslánc-nézetet)");
+
+define("FORUM_LAN_URL_REWRITE_LABEL", "SEF fórum URL-ek (fejlesztés alatt)");
 define("FORUM_LAN_URL_REWRITE_DESCR", "Példák:<br>FEJLESZTÉS ALATT");
--- Hungarian/e107_plugins/forum/languages/Hungarian/Hungarian_search.php
+++ Hungarian/e107_plugins/forum/languages/Hungarian/Hungarian_search.php
@@ -4,6 +4,6 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("FOR_SCH_LAN_2", "Válassza ki a fórum");
-define("FOR_SCH_LAN_4", "Egész post");
-define("FOR_SCH_LAN_5", "Részeként-ból téma");
+define("FOR_SCH_LAN_2", "Válassz fórumot");
+define("FOR_SCH_LAN_4", "Teljes bejegyzésben");
+define("FOR_SCH_LAN_5", "Egy adott témában");
