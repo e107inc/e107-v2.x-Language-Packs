563cdae4630d795dd065392bb8857d8ced9a3868
Lóna Lore <lonalore@freemail.hu>
2015-09-11 11:38:48 +0200

    reviewed translations for log plugin

--- Hungarian/e107_plugins/log/languages/Hungarian_log.php
+++ Hungarian/e107_plugins/log/languages/Hungarian_log.php
@@ -4,7 +4,7 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("LAN_AL_STAT_01", "Statisztika - reset");
-define("LAN_AL_STAT_02", "Statisztika - beállításokat megváltoztatta");
-define("LAN_AL_STAT_03", "Statisztika - oldalak eltávolítva");
-define("LAN_AL_STAT_04", "Statisztika - történelmi adatok eltávolítása");
+define("LAN_AL_STAT_01", "Statisztika - nullázás");
+define("LAN_AL_STAT_02", "Statisztika - beállítások megváltoztak");
+define("LAN_AL_STAT_03", "Statisztika - oldalak törölve");
+define("LAN_AL_STAT_04", "Statisztika - naplózás törölve");
