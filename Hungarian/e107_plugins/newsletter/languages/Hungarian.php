27089302e1ec67384eb9b8da3a1e1a3cd457b169
Lóna Lore <lonalore@freemail.hu>
2015-09-11 13:07:33 +0200

    reviewed translations for newsletter plugin

--- Hungarian/e107_plugins/newsletter/languages/Hungarian.php
+++ Hungarian/e107_plugins/newsletter/languages/Hungarian.php
@@ -5,15 +5,17 @@
 // https://github.com/e107inc/multilan
 
 define("NLLAN_MENU_CAPTION", "Hírlevél");
-define("NLLAN_48", "Előfizetett ezt a hírlevelet - Ha szeretne leiratkozni kérjük kattintson az alábbi gombra.");
-define("NLLAN_49", "Biztos, hogy azt szeretné, hogy ENSZ-iratkozzon fel ezt a hírlevelet?");
-define("NLLAN_50", "Gombra kattintva iratkozzon fel (az előfizetés cím az");
+
+define("NLLAN_48", "Hírlevélről való leiratkozás - a hírlevél lemondásához katt a lenti gombra.");
+define("NLLAN_49", "Valóban szeretnél leiratkozni a hírlevélről?");
+define("NLLAN_50", "Katt a gombra a feliratkozáshoz (feliratkozó címed a következő)");
 define("NLLAN_51", "Leiratkozás");
 define("NLLAN_52", "Feliratkozás");
-define("NLLAN_53", "Biztosan azt szeretné, hogy iratkozzon fel a hírlevél?");
+define("NLLAN_53", "Valóban szeretnél feliratkozni a hírlevélre?");
+
 define("NLLAN_67", "Archívum – áttekintés");
-define("NLLAN_68", "Érvénytelen paraméter meghatározása");
-define("NLLAN_69", "Nem küldött hírlevelek elérhető.");
-define("NLLAN_70", "Nem létezik a kiválasztott hírlevél");
+define("NLLAN_68", "Érvénytelen paraméter lett meghatározva");
+define("NLLAN_69", "Nem elérhető elküldött hírlevél.");
+define("NLLAN_70", "A kiválasztott hírlevél nem létezik");
 define("NLLAN_71", "Vissza");
 define("NLLAN_72", "Archivum megtekintése");
