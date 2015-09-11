f93ec0f6014f9823e38f23727c92af4b87c11649
Lóna Lore <lonalore@freemail.hu>
2015-09-11 09:37:14 +0200

    reviewed translations for import plugin

--- Hungarian/e107_plugins/import/languages/Hungarian_global.php
+++ Hungarian/e107_plugins/import/languages/Hungarian_global.php
@@ -4,5 +4,5 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("LAN_PLUGIN_IMPORT_NAME", "E107 importálása");
-define("LAN_PLUGIN_IMPORT_DESCRIPTION", "Adatok importálása a Wordpress, Joomla, Drupal, Blogpost, RSS és egyéb formátumok.");
+define("LAN_PLUGIN_IMPORT_NAME", "E107 importálás");
+define("LAN_PLUGIN_IMPORT_DESCRIPTION", "Adatok importálása Wordpress-ből, Joomla-ból, Drupal-ból, Blogpost-ból, vagy RSS és egyéb formátumokból.");
