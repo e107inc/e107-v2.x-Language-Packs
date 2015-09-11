eeeba453825a1a1774f54053efaf4533097c7d82
Lóna Lore <lonalore@freemail.hu>
2015-09-11 15:52:28 +0200

    reviewed translations for trackback plugin

--- Hungarian/e107_plugins/trackback/languages/Hungarian_admin_trackback.php
+++ Hungarian/e107_plugins/trackback/languages/Hungarian_admin_trackback.php
@@ -4,10 +4,10 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("TRACKBACK_L7", "Hatékonnyá tesz trackback");
-define("TRACKBACK_L8", "Trackback URL szövege");
-define("TRACKBACK_L10", "Trackback-beállítások");
-define("TRACKBACK_L11", "Trackback cím részére ez felad:");
-define("TRACKBACK_L12", "Nincsenek Visszakövetések ehhez a cikkhez");
-define("TRACKBACK_L13", "Mérsékelt trackbacks");
+define("TRACKBACK_L7", "Trackback aktiválása");
+define("TRACKBACK_L8", "Trackback URL szöveg");
+define("TRACKBACK_L10", "Trackback beállítások");
+define("TRACKBACK_L11", "Trackback cím a hírhez:");
+define("TRACKBACK_L12", "Nincsenek trackback-ek");
+define("TRACKBACK_L13", "Trackback-ek moderálása");
 define("TRACKBACK_L16", "Trackback");
