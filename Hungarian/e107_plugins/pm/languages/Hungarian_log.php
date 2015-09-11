68a8a10eb551b2ce1de68970c5135093f1db02f3
Lóna Lore <lonalore@freemail.hu>
2015-09-11 13:49:01 +0200

    reviewed translations for pm plugin

--- Hungarian/e107_plugins/pm/languages/Hungarian_log.php
+++ Hungarian/e107_plugins/pm/languages/Hungarian_log.php
@@ -4,13 +4,13 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("LAN_AL_PM_ADM_01", "PM: Alapértelmezett beállítások használt");
-define("LAN_AL_PM_ADM_02", "PM: Prefs frissítve");
-define("LAN_AL_PM_ADM_03", "PM: DB karbantartás teljes");
-define("LAN_AL_PM_ADM_04", "PM: DB karbantartási kezdett");
-define("LAN_AL_PM_ADM_05", "PM: Limit hozzáadott");
-define("LAN_AL_PM_ADM_06", "PM: Fiókszám korlátozása frissítve");
+define("LAN_AL_PM_ADM_01", "PM: Alapértelmezett beállítások használata");
+define("LAN_AL_PM_ADM_02", "PM: Beállítások frissítve");
+define("LAN_AL_PM_ADM_03", "PM: DB karbantartás befejeződött");
+define("LAN_AL_PM_ADM_04", "PM: DB karbantartási elkezdődött");
+define("LAN_AL_PM_ADM_05", "PM: Limit hozzáadva");
+define("LAN_AL_PM_ADM_06", "PM: Limit frissítve");
 define("LAN_AL_PM_ADM_07", "PM: Limit törölve");
-define("LAN_AL_PM_ADM_08", "PM: Hiba létrehozása adatok korlátozása");
-define("LAN_AL_PM_ADM_09", "PM: Hiba korlátoz adatainak frissítése");
-define("LAN_AL_PM_ADM_10", "PM: Hiba törlése adatok korlátozása");
+define("LAN_AL_PM_ADM_08", "PM: Hiba lépett fel a limit hozzáadásakor");
+define("LAN_AL_PM_ADM_09", "PM: Hiba lépett fel a limit frissítésekor");
+define("LAN_AL_PM_ADM_10", "PM: Hiba lépett fel a limit törlésekor");
