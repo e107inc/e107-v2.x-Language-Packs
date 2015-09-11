442c9d5e6a32a6ea53e51d1059a5adb42f2b5aa8
Lóna Lore <lonalore@freemail.hu>
2015-09-11 12:54:36 +0200

    reviewed translations for newsfeed plugin

--- Hungarian/e107_plugins/newsfeed/languages/Hungarian_newsfeed.php
+++ Hungarian/e107_plugins/newsfeed/languages/Hungarian_newsfeed.php
@@ -4,10 +4,10 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("NFLAN_29", "Elérhető hírcsatornák");
-define("NFLAN_31", "Vissza a listához hírcsatorna");
-define("NFLAN_33", "Kiadás dátuma:");
+define("NFLAN_29", "Elérhető hírforrások");
+define("NFLAN_31", "Vissza a hírforrás lisához");
+define("NFLAN_33", "Közzététel dátuma:");
 define("NFLAN_34", "nem ismert");
-define("NFLAN_38", "Aktualitások");
+define("NFLAN_38", "Címsorok");
 define("NFLAN_39", "Részletek");
-define("NFLAN_48", "Nem lehet menteni a nyers adatokat az adatbázisban.");
+define("NFLAN_48", "Nem lehet elmenteni ezeket az adatokat az adabázisba.");
