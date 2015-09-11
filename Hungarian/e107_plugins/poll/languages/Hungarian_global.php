dee52b982b8e1ce65c11f311353a89f2a90223c6
Lóna Lore <lonalore@freemail.hu>
2015-09-11 15:14:23 +0200

    reviewed translations for poll plugin

--- Hungarian/e107_plugins/poll/languages/Hungarian_global.php
+++ Hungarian/e107_plugins/poll/languages/Hungarian_global.php
@@ -4,5 +4,5 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("LAN_PLUGIN_POLL_NAME", "Közvélemény-kutatás");
-define("LAN_PLUGIN_POLL_DESCRIPTION", "A szavazás plugin lehetővé teszi, hogy meghatározza közvélemény-kutatások, akár egy menü vagy a fórum-post.");
+define("LAN_PLUGIN_POLL_NAME", "Szavazás");
+define("LAN_PLUGIN_POLL_DESCRIPTION", "Szavazások létrehozására használatos plugin.");
