9cde05540c2d84175d6edfc44f82ee2df49e8d2c
Lóna Lore <lonalore@freemail.hu>
2015-09-11 10:14:46 +0200

    reviewed translations for linkwords plugin

--- Hungarian/e107_plugins/linkwords/languages/Hungarian_admin.php
+++ Hungarian/e107_plugins/linkwords/languages/Hungarian_admin.php
@@ -4,52 +4,54 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("LWLAN_4", "Nem link szöveg határozták még meg.");
+define("LWLAN_4", "Nincs link szó definiálva.");
 define("LWLAN_5", "Szavak");
 define("LWLAN_6", "Link");
 define("LWLAN_7", "Aktív?");
-define("LWLAN_11", "Meglévő Linkwords");
-define("LWLAN_18", "Biztosan törli ezt a linkword?");
-define("LWLAN_20", "Képtelen-hoz talál a linkword bejegyzés.");
-define("LWLAN_21", "Word automatikus kapcsolása (vagy vesszővel elválasztott szavak)");
-define("LWLAN_22", "Aktiválni?");
-define("LWLAN_23", "Felügyeleti Linkwords");
+define("LWLAN_11", "Jelenlegi linkszavak");
+define("LWLAN_18", "Biztosan törölni szeretnéd ezt a linkszó elemet?");
+define("LWLAN_20", "Nem található ilyen linkszó");
+define("LWLAN_21", "Szó az automatikus linkhez (vesszővel elválasztva több is megadható)");
+define("LWLAN_22", "Aktivál?");
+define("LWLAN_23", "Aktiválás?");
 define("LWLAN_24", "Szavak kezelése");
-define("LWLAN_26", "Ahhoz, hogy linkwords területek");
-define("LWLAN_27", "Ez a 'context' a megjelenített szöveg");
-define("LWLAN_28", "Oldalak linkwords letiltása");
-define("LWLAN_29", "Azonos formátumú Menüvezérlés láthatóságát. Egy mérkőzés soronként. Adjon meg egy részleges, vagy teljes URL-címét. A végén '!' pontos egyezés a link végén része");
-define("LWLAN_31", "Linkword hozzáadása/szerkesztése");
-define("LWLAN_33", "Területen megnevezése");
-define("LWLAN_34", "Cikk összefoglalók");
-define("LWLAN_35", "Szövegtörzs");
+define("LWLAN_26", "Területek, ahol engedélyezed a linkszavak használatát");
+define("LWLAN_27", "A megjelenítendő szövegrészek kontextusai");
+define("LWLAN_28", "Oldalak, melyeken letiltva a link szavak");
+define("LWLAN_29", "Néhány forma, mint menü láthatóság szabályozás. Egy beillesztés/sor. Az előírás szerinti relatív vagy teljes URL. A végén '!' jel, a link végének pontos meghatározásához");
+define("LWLAN_31", "Linkszavak hozzáadása/szerkesztése");
+define("LWLAN_33", "Címek");
+define("LWLAN_34", "Összegzések");
+define("LWLAN_35", "Szövegtörzsek");
 define("LWLAN_36", "Leírások (linkek stb)");
-define("LWLAN_40", "Felhasználó által beírt címek (pl. fórum)");
-define("LWLAN_41", "Felhasználó által megadott szöveg (pl. fórum)");
-define("LWLAN_50", "Tooltip");
+define("LWLAN_40", "Felhasználó által megadott címek (pl. fórum)");
+define("LWLAN_41", "Felhasználó által megadott szövegtörzsek (pl. fórum)");
+
+define("LWLAN_50", "Buboréksúgó");
 define("LWLAN_51", "Inaktív");
-define("LWLAN_52", "Linkword csak");
-define("LWLAN_53", "Tooltip csak");
-define("LWLAN_54", "Linkword és Tooltip");
+define("LWLAN_52", "Csak a linkszó");
+define("LWLAN_53", "Csak buboréksúgó");
+define("LWLAN_54", "Linkszó és buboréksúgó");
 define("LWLAN_55", "Link megnyitása új ablakban");
 define("LWLAN_56", "Megnyitás új lapon");
-define("LWLAN_58", "Előre gyártott akasztó 0,8 formátumba konvertált");
-define("LWLAN_59", "Ajax engedélyezéséhez");
-define("LWLAN_60", "LW-AZONOSÍTÓ");
-define("LWLAN_62", "Tooltip azonosító (LW-azonosító)");
+define("LWLAN_58", "\"Pref hook\" konvertálása v2 formátumra");
+define("LWLAN_59", "Ajax engedélyezése");
+define("LWLAN_60", "LW ID");
+define("LWLAN_62", "Buboréksúgó ID (LW ID)");
 define("LWLAN_63", "Csak pozitív egész számok");
-define("LWLAN_64", "Elnyomja az aktuális oldal link");
-define("LWLAN_65", "Ha be van jelölve, elnyomja kattintható linket, ha rámutat arra, hogy aktuális oldal");
-define("LAN_LW_HELP_00", "Linkwords Súgó");
+define("LWLAN_64", "Link elhagyása a jelenlegi oldalon");
+define("LWLAN_65", "Ha bejelölöd, elhgyja a kattintható linket, ha az az aktuális oldalra mutat");
+
+define("LAN_LW_HELP_00", "Linkszó Súgó");
 define("LAN_LW_HELP_01", "
-  <b>Ahhoz, hogy a területek</b><br>sok területen a szöveg van egy kapcsolódó \'context\', és linkwords csak jelenik meg területek megfelelő összefüggésben.<br><br>
-  <b>Linkwords letiltása</b><br>Linkwords megszűnhet az adott oldalak vagy lapok megfelelő mintát. Adja meg, ezek itt (a szintaxisa ugyanaz mint menük), soronként egy minta. 
-  Ha a minta végződik \ '! \', ez megfelel a query\ \'end', és általában pontos egyezést. Egyébként úgy illeszkednek, bármilyen URL-t, amely a megadott karakterláncot tartalmazza.<br>Vegye figyelembe, hogy linkwords <i>soha nem</i> jelenik meg admin oldalak.<br><br>
-  <b>Ajax engedélyezéséhez</b><br>A tooltips Ajax segítségével kap információt a megjelenítéshez. Ez általában szükséges bizonyos szokás kódexek.<br><br>
-  <b>Elnyomja az aktuális oldal link</b><br>Általában az értelmetlen a felhasználónak, hogy kattintson a linkre, ha they\ 're-már az oldalon. Jelölje be a négyzetet, hogy távolítsa el a lehetőség,<br>");
-define("LAN_LW_HELP_02", "A szavak váló clickable láncszemek, vagy amely mouseover, itt szöveget jeleníthetünk meg meghatározásához<br><br>
-  <b><u>Word link</u></b><br>Ez a kis-és nagybetűket. Több szó hozzárendelése az azonos linkek és tippek akkor ezeket vesszővel kell elválasztani (szóköz nélkül)<br><br>
-  <b><u>Link</u></b><br>Határozza meg egy clickable láncszem itt. Ha a külső hivatkozást, [i] kell [/ i] \'http://\ kezdődik'. Ha ez egy linket ezen az oldalon belül, a normál {e_XXX}-állandók használhatók.<br><br>
-  <b><u>Tooltip</u></b><br>Ez határozza meg a szöveg jelenik meg, amikor a user\ 's egér halad át a szót.<br><br>
-  <b><u>LW-azonosító (Tooltip-azonosító)</u></b><br><br>Ez határozza meg a választható numerikus azonosító használható Ajax feldolgozása. Ha a mező üres, az adatbázis-rekord számú használatos.<br><br>
-  <b><u>Aktiválni?</u></b><br>Határozza meg, mely beállítások aktívak.");
+  <b>Engedélyezési területek</b><br>azok a szöveg-területek, ahol a linkszavak engedélyezve lesznek.<br><br>
+  <b>Tiltott területek</b><br>azok a területek, ahol a linkszavak használata nem lesz engedélyezve. Ugyanolyan szintaxisban add meg őket, mint a menüknél, soronként egy útvonal-minta.
+  Ha a minta '!' jellel végződik, akkor egy konkrét útvonalként lesz értelmezve, máskülönben a szövegrészen belül is lehetnek találatok.<br><br>
+  <b>Ajax engedélyezése</b><br>A buboréksúgó tartalma ajax kérésen keresztül kapja az értéket.<br><br>
+  <b>Link elhagyása a jelenlegi oldalon</b><br>Azon szavak kiszűrése, melyek az aktuális oldalhoz vannak rendelve, tehát azon linkek nem lesznek megjelenítve, melyek önmagukra mutatnak.<br>");
+define("LAN_LW_HELP_02", "Azon szavak meghatározása, melyek kattintható linkekké lesznek alakítva, vagy amikre buboréksúgó lesz helyezve<br><br>
+  <b><u>Szavak linkekké alakítása</u></b><br>A megadásnál ügyelj arra, hogy a kis- és nagybetű nem azonos. Több szó esetén vesszővel elválasztva add meg őket.<br><br>
+  <b><u>Link</u></b><br>Kattintható linkek megadása. Ha külső hivatkozást adsz meg, a hivatkozásnak http://-rel kell kezdődnie. Ha belső hivatkozást szeretnél, úgy a {e_XXX} konstansok használhatóak.<br><br>
+  <b><u>Buboréksúgó</u></b><br>Egy buboréksúgó jelenik meg a megadott szó fölött, ha az egérmutató a szó fölé kerül.<br><br>
+  <b><u>LW ID (Buboréksúgó ID)</u></b><br><br>Optimális, numerikus ID meghatározása az Ajax-kérésekhez. Üresen hagyva az adatbázisban található ID lesz használva.<br><br>
+  <b><u>Aktivál?</u></b><br>Meghatározható, mely beállítások legyenek aktívak.");
