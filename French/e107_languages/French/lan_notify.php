8bd498c8162b7631146692d21c7f62bce8cf05c0
OTroccaz <olivier.troccaz@univ-rennes1.fr>
2015-09-22 16:15:11 +0200

    New french translations

--- French/e107_languages/French/lan_notify.php
+++ French/e107_languages/French/lan_notify.php
@@ -4,18 +4,18 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("NT_LAN_US_1", "Inscription de l'utilisateur");
+define("NT_LAN_US_1", "Inscription utilisateur");
 define("NT_LAN_UV_1", "Inscription utilisateur vérifiée");
-define("NT_LAN_UV_2", "ID de l'utilisateur :");
-define("NT_LAN_UV_3", "Nom de connexion utilisateur :");
-define("NT_LAN_UV_4", "L'utilisateur IP :");
+define("NT_LAN_UV_2", "ID utilisateur : ");
+define("NT_LAN_UV_3", "Nom de connexion utilisateur : ");
+define("NT_LAN_UV_4", "IP utilisateur : ");
 define("NT_LAN_LI_1", "Utilisateur connecté");
-define("NT_LAN_LO_1", "L'utilisateur s'est déconnecté");
-define("NT_LAN_LO_2", "déconnecté du site");
-define("NT_LAN_FL_1", "Interdiction de l'inondation");
-define("NT_LAN_FL_2", "Adresse IP interdite d'inondations");
-define("NT_LAN_SN_1", "News point soumis");
-define("NT_LAN_ML_1", "En vrac email envoyez complet");
-define("NT_LAN_NU_1", "Mise à jour");
-define("NT_LAN_ND_1", "News point supprimés");
-define("NT_LAN_ND_2", "Id d'élément supprimé nouvelles");
+define("NT_LAN_LO_1", "Utilisateur déconnecté");
+define("NT_LAN_LO_2", " déconnecté du site");
+define("NT_LAN_FL_1", "Exclusion pour cause de flood");
+define("NT_LAN_FL_2", "Adresse IP exclue pour cause de flood");
+define("NT_LAN_SN_1", "Article soumis");
+define("NT_LAN_ML_1", "Mail en vrac envoyé complètement");
+define("NT_LAN_NU_1", "Mis(e) à jour");
+define("NT_LAN_ND_1", "Article supprimé");
+define("NT_LAN_ND_2", "ID article supprimé");
--- French/e107_languages/French/lan_user_select.php
+++ French/e107_languages/French/lan_user_select.php
@@ -4,9 +4,9 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("US_LAN_1", "Sélectionnez utilisateur");
-define("US_LAN_2", "Sélectionnez la classe d'utilisateur");
+define("US_LAN_1", "Sélectionner un utilisateur");
+define("US_LAN_2", "Sélectionner un groupe d'utilisateurs");
 define("US_LAN_3", "Tous les utilisateurs");
-define("US_LAN_4", "Trouver le nom d'utilisateur");
-define("US_LAN_5", "Utilisateur (s) trouvés");
+define("US_LAN_4", "Trouver un nom d'utilisateur");
+define("US_LAN_5", "Utilisateur(s) trouvé(s)");
 define("US_LAN_6", "Recherche");
