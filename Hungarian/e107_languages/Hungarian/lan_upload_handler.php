12cc059b6900673c2792bcbef67d19d390794f16
Gábor <yesszus@gmail.com>
2016-03-14 13:14:53 +0100

    Update lan_upload_handler.php

--- Hungarian/e107_languages/Hungarian/lan_upload_handler.php
+++ Hungarian/e107_languages/Hungarian/lan_upload_handler.php
@@ -5,20 +5,21 @@
 // https://github.com/e107inc/multilan
 
 define("LANUPLOAD_1", "A fájltípus");
-define("LANUPLOAD_2", "nem engedélyezett, törölve lett.");
-define("LANUPLOAD_3", "Sikeresen feltöltve");
+define("LANUPLOAD_2", "nem engedélyezett, törölve lett!");
+define("LANUPLOAD_3", "Sikeresen feltöltve!");
 define("LANUPLOAD_4", "A célkönyvtár nem létezik, vagy nem írható. (chmod 777)");
-define("LANUPLOAD_5", "A file túllépte a php.ini -ben meghatározott maximális méretet.");
+define("LANUPLOAD_5", "A fájl túllépte a php.ini -ben meghatározott maximális méretet.");
 define("LANUPLOAD_6", "A feltöltött fájl túllépte a MAX_FILE_SIZE értékét.");
 define("LANUPLOAD_7", "A fájl csak részben lett feltöltve.");
-define("LANUPLOAD_8", "Nem lett fájl feltöltve.");
-define("LANUPLOAD_9", "Feltöltött fileméret: 0 byte");
+define("LANUPLOAD_8", "A fájl nem lett feltöltve!");
+define("LANUPLOAD_9", "A feltöltött fájlméret: 0 bájt");
 define("LANUPLOAD_10", "A feltöltés nem sikerült [Duplikált fájlnév] - Már van ilyen nevű fájl.");
-define("LANUPLOAD_11", "A fájl nem lett feltöltve. Fájlnév: ");
+define("LANUPLOAD_11", "A fájl nem lett feltöltve! Fájlnév: ");
 define("LANUPLOAD_13", "Hiányzik az ideiglenes mappa");
-define("LANUPLOAD_14", "Fájl írás hibás");
-define("LANUPLOAD_15", "Feltöltés nem engedélyezett");
+define("LANUPLOAD_14", "Fájl írás hiba!");
+define("LANUPLOAD_15", "A feltöltés nincs engedélyezve!");
 define("LANUPLOAD_16", "Ismeretlen hiba");
-define("LANUPLOAD_17", "Feltöltött fájl érvénytelen fájl név");
+define("LANUPLOAD_17", "A feltöltött fájl érvénytelen fájl név");
 define("LANUPLOAD_18", "A feltöltött fájl túllépte a megengedett határértéket.");
 define("LANUPLOAD_19", "Túl sok a feltöltött fájl - többlet törölve.");
+?>
