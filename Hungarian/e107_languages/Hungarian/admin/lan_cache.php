30d03fb027e34bd2a34ce38993720429d444f64f
Gábor <yesszus@gmail.com>
2016-03-14 13:28:11 +0100

    Update lan_cache.php

--- Hungarian/e107_languages/Hungarian/admin/lan_cache.php
+++ Hungarian/e107_languages/Hungarian/admin/lan_cache.php
@@ -1,17 +1,21 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/02/23 11:45:52
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 
 define("CACLAN_1", "Gyorstárazás állapota");
 define("CACLAN_2", "Beállítások mentése");
 define("CACLAN_3", "Gyorsítótár-kezelő");
 define("CACLAN_5", "Tartalomak gyorsítótárának ürítése");
 define("CACLAN_6", "Gyorsítótár kiürítve");
-
 define("CACLAN_10", "A cache könyvtár nem írható. Ellenőrizd, hogy a szerver megfelelő jogosultságokkal rendelkezik a cache mappán. Ha nem, adj CHMOD 0777-et a mappára");
-
 define("CACLAN_11", "Tartalomak gyorstárazása");
 define("CACLAN_12", "Rendszerbeállítások gyorstárazása");
 define("CACLAN_13", "A weboldal által generált tartala gyorstárazása. Ide tartozik minden olyan tartalom (html), amit látsz a weboldalon.");
@@ -20,7 +24,6 @@ define("CACLAN_16", "Rendszerbeállítások gyorsítótárának ürítése");
 define("CACLAN_17", "Jelenlegi tartalom");
 define("CACLAN_18", "fájl");
 define("CACLAN_19", "fájl");
-
 define("CACLAN_20", "Adatbázis-struktúra gyorstárazása");
 define("CACLAN_21", "Az adatbázis táblák információinak gyorstárazása, melyek szükségesek a különböző core rutinokhoz. A weboldal által generált tartalmi rész nem tartozik bele.");
 define("CACLAN_22", "Thumbnail-ek (bélyegképek) gyorstárazása");
@@ -29,3 +32,7 @@ define("CACLAN_24", "Adatbázis-struktúra gyorsítótárának ürítése");
 define("CACLAN_25", "Thumbnail-ek (bélyegképek) gyorsítótárának ürítése");
 define("CACLAN_26", "Összes gyorsítótár ürítése");
 define("CACLAN_27", "Böngésző gyorsítótárának ürítése");
+define("CACLAN_28", "JS/CSS Cache");
+define("CACLAN_29", "A cache javascript fájlok és CSS fájlok megerősítése.");
+define("CACLAN_30", "JS/CSS Cache Ürítése");
+?>
