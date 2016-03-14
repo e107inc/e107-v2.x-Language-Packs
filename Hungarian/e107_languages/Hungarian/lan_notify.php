d6839a9b35218716b9eaa797f87eb8810f4f11f0
Gábor <yesszus@gmail.com>
2016-03-14 12:58:10 +0100

    Update lan_notify.php

--- Hungarian/e107_languages/Hungarian/lan_notify.php
+++ Hungarian/e107_languages/Hungarian/lan_notify.php
@@ -12,9 +12,9 @@ define("NT_LAN_UV_3", "Felhasználó Bejelentkező Név: ");
 define("NT_LAN_UV_4", "Felhasználó IP: ");
 
 
-define("NT_LAN_LI_1", "Felhasználó bejelentkezett");
+define("NT_LAN_LI_1", "Felhasználói bejelentkezés");
 
-define("NT_LAN_LO_1", "Felhasználó kijelentkezett");
+define("NT_LAN_LO_1", "Felhasználói kijelentkezés");
 define("NT_LAN_LO_2", " kijelentkezett az oldalról");
 
 define("NT_LAN_FL_1", "Kitiltás");
@@ -22,9 +22,10 @@ define("NT_LAN_FL_2", "Kitiltott IP cím");
 
 define("NT_LAN_SN_1", "Beküldött hír");
 
-define("NT_LAN_ML_1", "Tömeges email küldés kész");
+define("NT_LAN_ML_1", "Tömeges email küldés kész!");
 
-define("NT_LAN_NU_1", "Frissítve");
+define("NT_LAN_NU_1", "Frissítve!");
 
-define("NT_LAN_ND_1", "Hír törölve");
+define("NT_LAN_ND_1", "A hír törölve!");
 define("NT_LAN_ND_2", "Törölt hír ID");
+?>
