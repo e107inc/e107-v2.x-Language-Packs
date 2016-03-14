3a9a70575553c3ae9c6b3779fa687c33ca0d79b3
Gábor <yesszus@gmail.com>
2016-03-14 13:19:08 +0100

    Update lan_userclass.php

--- Hungarian/e107_languages/Hungarian/lan_userclass.php
+++ Hungarian/e107_languages/Hungarian/lan_userclass.php
@@ -10,10 +10,11 @@ define("UC_LAN_2", "Senki (inaktív)");
 define("UC_LAN_3", "Tagok");
 define("UC_LAN_4", "Csak olvasási jog");
 define("UC_LAN_5", "Adminisztrátor");
-define("UC_LAN_6", "Fő Adminisztrátor");
+define("UC_LAN_6", "Főadminisztrátor");
 define("UC_LAN_7", "Fórum Moderátorok");
 define("UC_LAN_8","Adminok és Moderátorok");
 define("UC_LAN_9","Új Tagok");
 define("UC_LAN_10", "Kereső Robotok");
 define("UC_LAN_INVERT", "Nem --CLASS--");
 define("UC_LAN_INVERTLABEL", "Mindenki, kivéve..");
+?>
