2d3dd44dd928940115115db3610486af421cc8d3
Gábor <yesszus@gmail.com>
2016-03-14 13:33:11 +0100

    Update lan_db.php

--- Hungarian/e107_languages/Hungarian/admin/lan_db.php
+++ Hungarian/e107_languages/Hungarian/admin/lan_db.php
@@ -1,9 +1,14 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/02/27 21:28:48
+|
+|        $Author: Torrentor $
++---------------------------------------------------------------+
+*/
 define("DBLAN_1", "Core beállítások elmentve az adatbázisban.");
 define("DBLAN_4", "Adatbázis érvényességének ellenőrzése");
 define("DBLAN_5", "Érvényesség ellenőrzése");
@@ -12,7 +17,7 @@ define("DBLAN_7", "Adatbázis optimalizálása");
 define("DBLAN_8", "Core beállítások mentése");
 define("DBLAN_9", "Core lementése");
 define("DBLAN_10", "Adatbázis eszközök");
-define("DBLAN_11", "MySQL adatbázis");
+define("DBLAN_11", "A  [x] MySQL adatbázis optimalizálva");
 define("DBLAN_12", "optimalizálva");
 define("DBLAN_15", "Adatbázis frissítések keresése");
 define("DBLAN_16", "Frissítések keresése");
@@ -31,43 +36,37 @@ define("DBLAN_30", "Ha egy bővítmény egy hibát mutat, ellenőrizd a PHP kezd
 define("DBLAN_31", "Viszgálat kész");
 define("DBLAN_33", "Elérhetetlen");
 define("DBLAN_34", "Nem ellenörzött");
-
 define("DBLAN_35", "e107 adatbázis bejegyzések ellenőrzése");
 define("DBLAN_36", "Adatbázis bejegyzések ellenőrzése");
 define("DBLAN_37", "Táblák ellenőrzése");
 define("DBLAN_38", "Ellenőrzés indítása");
 define("DBLAN_39", "Adatbázis bejegyzések ellenőrzése");
-define("DBLAN_40", "Bejegyzés ellenőrizve : ");
+define("DBLAN_40", "Bejegyzés ellenőrizve :");
 define("DBLAN_41", "tábla");
 define("DBLAN_43", "megjegyzés");
 define("DBLAN_45", "Id nem található!");
 define("DBLAN_46", "Tábla nem található!");
 define("DBLAN_49", "Jelenleg nincs bejegyzés a táblában, nincs mit ellenőrizni");
 define("DBLAN_50", "Sql bejegyzés ellenőrzése");
-
 define("DBLAN_51", "Végrehajtás kiválasztva");
-define("DBLAN_52", "Duplikáció törlése"); //plugin scan
+define("DBLAN_52", "Duplikáció törlése");
 define("DBLAN_53", "Válaszd ki a műveletet.");
 define("DBLAN_54", "Nincs hiba.");
 define("DBLAN_55", "Válassz a Shortcode/Könyvtár felülírása ellenőrzéséhez az új shortcode-hoz");
 define("DBLAN_56", "Felülírások ellenőrzése");
-define("DBLAN_57", "Shortcode felülírás lista kiválasztva ehhez ");
-
+define("DBLAN_57", "Shortcode felülírás lista kiválasztva ehhez");
 define("DBLAN_58", "Adatok exportálása");
 define("DBLAN_59", "Adatok importálása");
-
 define("DBLAN_60", "Fájl mentés kész!");
 define("DBLAN_61", "Adatbázis mentés indítása...");
 define("DBLAN_62", "Adatbázis mentés kész!");
 define("DBLAN_63", "A teljes oldal mentés kész.");
-
 define("DBLAN_64", "Adatbázis karakterkészlet ellenőrzése");
 define("DBLAN_65", "Karakterkódolás ellenőrzése");
 define("DBLAN_66", "Helyes fájl és könyvtár jogosultságok");
 define("DBLAN_67", "Helyes jogosultságok");
 define("DBLAN_68", "Adatbázis mentés, Fájlok és könyvtárak");
 define("DBLAN_69", "Oldal lementése");
-
 define("DBLAN_70", "Adatbázis (SQL) dump fájl létrehozása, valamint zip fájl létrehozása a harmadik féltől származó bővítményekkel, sminkekkel, média fájlokkal és a rendszer naplófájljaival.");
 define("DBLAN_71", "Kérlek várj...");
 define("DBLAN_72", "Könyvtár és fájl jogosultságok frissítve");
@@ -76,12 +75,10 @@ define("DBLAN_74", "Javítás a szerveren");
 define("DBLAN_75", "Adatbázis létrehozása");
 define("DBLAN_76", "Adatbázis kiválasztása");
 define("DBLAN_77", "Nem lehet olvasni a core sql fájlt");
-
 define("DBLAN_78", "Tábla");
 define("DBLAN_79", "Motor");
 define("DBLAN_80", "Egybevetés");
 define("DBLAN_81", "Állapot");
-
 define("DBLAN_82", "Ez a funkció állandó jelleggel módosítja az adatbázis összes tábláját. ([database])");
 define("DBLAN_83", "Ez [b]ERŐSEN[/b] javasolt az első adatbázis mentéskor és az oldal karbantartás módja esetén.");
 define("DBLAN_84", "Megjegyzés:");
@@ -89,14 +86,12 @@ define("DBLAN_85", "Ez a folyamat több percig eltarthat az adatbázis méretét
 define("DBLAN_86", "Az átalakítás nem működik az egymáshoz kapcsolt tömböknél.");
 define("DBLAN_87", "Ügyelj arra, hogy pontosan kövesd a frissítési folyamat lépéseit.");
 define("DBLAN_88", "Core beállítások figyelmen kívül hagyása az átalakítás folyamatában korrupció lehetősége miatt.");
-
 define("DBLAN_89", "Adatbázis átalakítása");
 define("DBLAN_90", "Nem-UTF8 táblák átalakítása");
 define("DBLAN_91", "Kérlek várj...");
 define("DBLAN_92", "A táblák helyes karakter kiválasztást használnak.");
 define("DBLAN_93", "Adatbázis átalakítása UTF-8-ba kész.");
 define("DBLAN_94", "Győződj meg a következő sor létezéséről az e107_config.php fájlban:");
-
 define("DBLAN_95", "Beállítások exportálása");
 define("DBLAN_97", "Táblák");
 define("DBLAN_98", "Sorok");
@@ -105,12 +100,12 @@ define("DBLAN_100", "Útvonal, kép csomag és XML konvertálása ebbe:");
 define("DBLAN_101", "Fájl exportálása");
 define("DBLAN_102", "Beállítások exportálása");
 define("DBLAN_103", "Beillesztve");
-define("DBLAN_104", "Beillesztés sikertelen");
+define("DBLAN_104", "Sikertelen beillesztés");
 define("DBLAN_105", "Kötegelt shortcode-k:");
 define("DBLAN_106", "(üres)");
-
 define("DBLAN_107", "[folder] nem írható");
 define("DBLAN_108", "Létrehozva:");
 define("DBLAN_109", "Másolva:");
 define("DBLAN_110", "Nem lehet másolni:");
 define("DBLAN_111", "Úgy tűnik minden rendben van a kiválasztott táblával/táblákkal!");
+?>
