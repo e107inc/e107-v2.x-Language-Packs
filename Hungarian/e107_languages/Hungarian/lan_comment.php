a750921fbf6ef455dcdb03c32d4ce2229321f99c
Gábor <yesszus@gmail.com>
2016-03-14 12:37:56 +0100

    Update lan_comment.php

--- Hungarian/e107_languages/Hungarian/lan_comment.php
+++ Hungarian/e107_languages/Hungarian/lan_comment.php
@@ -1,15 +1,21 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/02/22 13:19:46
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("COMLAN_0", "[admin által tiltott]");
+define("COMLAN_0", "[admin által letiltva]");
 define("COMLAN_1", "Feloldás");
 define("COMLAN_2", "Tiltás");
-define("COMLAN_4", "Info");
+define("COMLAN_4", "Infó");
 define("COMLAN_5", "Hozzászólások...");
-define("COMLAN_6", "Hozzászólás küldéséhez be kell jelentkezned. Jelentkezz be, vagy kattints");
+define("COMLAN_6", "Hozzászólás beküldéséhez jelentkezz be, vagy kattints");
 define("COMLAN_7", "Főadminisztrátor");
 define("COMLAN_8", "Hozzászólás");
 define("COMLAN_9", "Hozzászólás beküldése");
@@ -20,20 +26,20 @@ define("COMLAN_16", "Felhasználónév:");
 define("COMLAN_100", "Hírek");
 define("COMLAN_101", "Szavazás");
 define("COMLAN_102", "Válasz erre:");
-define("COMLAN_103", "Cikk");
+define("COMLAN_103", "Hír");
 define("COMLAN_104", "Áttekintés");
 define("COMLAN_105", "Tartalom");
 define("COMLAN_106", "Letöltés");
-define("COMLAN_145", "Regisztrált: ");
+define("COMLAN_145", "Regisztrált:");
 define("COMLAN_194", "Vendég");
 define("COMLAN_195", "Regisztrált felhasználó");
-
-define("COMLAN_310", "Ez a felhasználónév már regisztrálva van. Ha mégis te vagy, kérlek jelentkezz be.");
+define("COMLAN_310", "Ez a felhasználónév már regisztrálva van.
+Ha mégis te vagy, kérlek jelentkezz be.");
 define("COMLAN_312", "Duplikált bejegyzés - nem lett elfogadva.");
 define("COMLAN_313", "Hely");
 define("COMLAN_314", "hozzászólások moderálása");
 define("COMLAN_315", "Visszakövetések");
-define("COMLAN_316", "Nincsenek visszakövetések ehhez a (hír-) bejegyzéshez.");
+define("COMLAN_316", "Nincs visszakövetés a (hír-) bejegyzéshez.");
 define("COMLAN_317", "Visszakövetések moderálása");
 define("COMLAN_318", "Hozzászólás szerkesztése");
 define("COMLAN_319", "szerkesztve");
@@ -51,20 +57,21 @@ define("COMLAN_331", "Jóváhagyás függőben");
 define("COMLAN_332", "Nem sikerült törölni a hozzászólást");
 define("COMLAN_333", "Hozzászólás jóvá lett hagyva");
 define("COMLAN_334", "Nem sikerült a hozzászólás jóváhagyása");
-define("COMLAN_335", "Jóváhagyott");
+define("COMLAN_335", "Jóváhagyva");
 define("COMLAN_336", "Kérlek, előbb írj valamit.");
 define("COMLAN_337", "Sikeresen frissítve.");
-
-define("COMLAN_400", "jóváhagyott");
-define("COMLAN_401", "blokkolt");
+define("COMLAN_400", "jóváhagyva");
+define("COMLAN_401", "blokkolva");
 define("COMLAN_402", "jóváhagyásra vár");
-
+define("COMLAN_403", "Hozzászólás írása...");
+define("COMLAN_404", "Engedélyez");
 define("COMLAN_TYPE_1", "Hírek");
 define("COMLAN_TYPE_2", "Letöltés");
 define("COMLAN_TYPE_3", "gyakran feltett kérdések");
 define("COMLAN_TYPE_4", "közvélemény-kutatás");
-define("COMLAN_TYPE_5", "Dokik");
+define("COMLAN_TYPE_5", "Dokumentum");
 define("COMLAN_TYPE_6", "bugtrack");
 define("COMLAN_TYPE_7", "ötletek");
-define("COMLAN_TYPE_8", "USERPROFILE");
+define("COMLAN_TYPE_8", "Felhasználó profilja");
 define("COMLAN_TYPE_PAGE", "Tartalom");
+?>
