3f4c8b75e2afc4471a4365ab5b9f5531d7e0c189
Gábor <yesszus@gmail.com>
2016-03-17 13:25:24 +0100

    Update lan_fileinspector.php

--- Hungarian/e107_languages/Hungarian/admin/lan_fileinspector.php
+++ Hungarian/e107_languages/Hungarian/admin/lan_fileinspector.php
@@ -4,7 +4,7 @@
 // Generated for e107 v2.x by the Multi-Language Plugin
 // https://github.com/e107inc/multilan
 
-define("FC_LAN_1", "File Ellenőrző");
+define("FC_LAN_1", "Fájl Ellenőrző");
 define("FC_LAN_2", "Ellenőrzési beállítások");
 define("FC_LAN_3", "Megtekintés");
 define("FC_LAN_4", "Összes fájl");
@@ -34,35 +34,35 @@ define("FR_LAN_4", "Core fájlok");
 define("FR_LAN_5", "Nem core fájlok");
 define("FR_LAN_6", "Összes fájl");
 define("FR_LAN_7", "Integritás ellenőrzés");
-define("FR_LAN_8", "Core fájlok ellenőrizve");
-define("FR_LAN_9", "Core fájlok hibásak");
+define("FR_LAN_8", "Ellenőrzött Core fájlok");
+define("FR_LAN_9", "Hibás Core fájlok");
 define("FR_LAN_10", "Elképzelhető, hogy a fájlok hibásak");
-define("FR_LAN_11", "A file sérült");
-define("FR_LAN_12", "Ez betudható a hibás zip file-nek, hiba jöhetett létre a kicsomagolás
+define("FR_LAN_11", "A fájl sérült");
+define("FR_LAN_12", "Ez betudható egy hibás zip fájlnak. Hiba jöhetett létre a kicsomagolás
 folyamán vagy az FTP feltöltés során. Próbáld meg újra feltölteni a hibás fájlt a szerverre
-és futtasd újra a File Ellenőrzőt, hogy megnézd megszünt-e a hiba.");
+és futtasd újra a Fájl Ellenőrzőt, hogy megnézd megszünt-e a hiba.");
 define("FR_LAN_13", "A fájl elavult");
 define("FR_LAN_14", "Ha a fájl egy régebbi e107 verzióból való és szerepel a fájlok között, akkor
-a frissebb e107 futásakor hibát jelezhet az integritás ellenőrzésekor. Természetesen, töltsd fel a file újabb verzióját.");
-define("FR_LAN_15", "A file módosítva lett");
-define("FR_LAN_16", "Ha módosítottad ezt a fájlt bárhol, akkor az integritás ellenőrzés nem lesz sikeres. Ha
+a frissebb e107 futásakor hibát jelezhet az integritás ellenőrzésekor. Természetesen, töltsd fel a fájl újabb verzióját.");
+define("FR_LAN_15", "A fájl módosítva lett");
+define("FR_LAN_16", "Ha bárhol módosítottad ezt a fájlt, akkor az integritás ellenőrzés nem lesz sikeres. Ha
 szándékosan módosítottad ezt a fájlt, nem kell idegeskedned és ne vedd figyelembe ezt az integritás ellenőrzési hibát. Azonban, ha
-a fájlt valaki más módosította az engedélyed nélkül, akkor tölts fel újra a helyes fájl verziót
+a fájlt valaki más módosította az engedélyed nélkül, akkor töltsd fel újra a helyes fájl verziót
 ehhez a fájlhoz.");
 define("FR_LAN_17", "Ha SVN használó vagy");
 define("FR_LAN_18", "Ha futtatod az ellenőrzést és SVN verziót használsz a stabil hivatalos e107 kiadás
 helyett, akkor tapasztalhatod, hogy sok fájl integritás ellenőrzése hibás lehet, ami abból adódik, hogy az utolsó CVS mentés óta már
 módosította a fejlesztő csapat az adott fájlt.");
 define("FR_LAN_19", "hibás fájlok");
-define("FR_LAN_20", "Összes file ellenőrizve");
+define("FR_LAN_20", "Összes fájl ellenőrizve");
 define("FR_LAN_21", "nincs");
 define("FR_LAN_22", "Hiányzó core fájlok");
 define("FR_LAN_23", "Nincs találat.");
 define("FR_LAN_24", "Elavult core fájlok");
-define("FR_LAN_25", "Integritás nem kiszámítható");
+define("FR_LAN_25", "Nem kiszámítható integritás");
 
 define("FR_LAN_26", "Figyelmeztetés! Ismert biztonsági rés felismerve!");
-define("FR_LAN_27", "Ezek a file-k, melyek ismertek, visszaélésre adhatnak lehetőséget és azonnal törölni kell.");
+define("FR_LAN_27", "Ezek a fájlok, melyek ismertek, visszaélésre adhatnak lehetőséget és azonnal törölni kell.");
 define("FR_LAN_28", "Ismert veszélyes fájlok");
 
 define("FR_LAN_29", "Összes fájl egyezés");
@@ -81,3 +81,4 @@ define("FS_LAN_9", "Pillanatkép");
 define("FS_LAN_10", "Pillanatkép kész");
 define("FS_LAN_11", "A pillanatkép sikeresen létrehozva.");
 define("FS_LAN_12", "Vissza a főoldalra");
+?>
