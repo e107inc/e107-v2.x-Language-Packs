3c5da476810f3c0983f0dd733ef891a5c93eb4c4
Lóna Lore <lonalore@freemail.hu>
2016-03-23 15:19:19 +0100

    Removed third-party plugin files... -_-

--- Hungarian/e107_plugins/nodejs_online/languages/Hungarian/Hungarian_admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Definition of hungarian strings for Admin UI.
- */
-
-define('LAN_NODEJS_ONLINE_ADMIN_01', 'Beállítások');
--- Hungarian/e107_plugins/nodejs_online/languages/Hungarian/Hungarian_front.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Definition of hungarian strings for area Frontend.
- */
-
-define('LAN_NODEJS_ONLINE_MENU_01', 'Online felhasználók');
