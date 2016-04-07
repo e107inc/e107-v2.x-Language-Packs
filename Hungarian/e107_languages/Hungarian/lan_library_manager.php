efb7d505518c4ddf27aa706d749ee7033d31fa44
Lóna Lore <lonalore@freemail.hu>
2016-04-07 12:26:54 +0200

    Fixed language file.

--- Hungarian/e107_languages/Hungarian/lan_library_manager.php
+++ Hungarian/e107_languages/Hungarian/lan_library_manager.php
@@ -10,17 +10,17 @@
 +---------------------------------------------------------------+
 */
 
-define("LAN_LIBRARY_MANAGER_01", "A(z) "[x]" library, mely a(z) "[y]" library függősége, nincs telepítve.");
-define("LAN_LIBRARY_MANAGER_02", "A(z) "[y]" library verziója ("[x]") nem kompatibilis a(z) "[z]" library-vel.");
-define("LAN_LIBRARY_MANAGER_03", "A(z) "[x]" library nem található.");
-define("LAN_LIBRARY_MANAGER_04", "A(z) "[x]" library verziója nem meghatározható.");
-define("LAN_LIBRARY_MANAGER_05", "A(z) "[y]" library verziója ("[x]") nem támogatott.");
-define("LAN_LIBRARY_MANAGER_06", "A(z) "[x]" változata a(z) "[y]" library-nak nem található.");
-define("LAN_LIBRARY_MANAGER_07", "hiányzó függőség");
-define("LAN_LIBRARY_MANAGER_08", "inkompatibilis függőség");
-define("LAN_LIBRARY_MANAGER_09", "nem található");
-define("LAN_LIBRARY_MANAGER_10", "meghatározhatatlan");
-define("LAN_LIBRARY_MANAGER_11", "nem támogatott");
+define('LAN_LIBRARY_MANAGER_01', 'A(z) "[x]" library, mely a(z) "[y]" library függősége, nincs telepítve.');
+define('LAN_LIBRARY_MANAGER_02', 'A(z) "[y]" library verziója ("[x]") nem kompatibilis a(z) "[z]" library-vel.');
+define('LAN_LIBRARY_MANAGER_03', 'A(z) "[x]" library nem található.');
+define('LAN_LIBRARY_MANAGER_04', 'A(z) "[x]" library verziója nem meghatározható.');
+define('LAN_LIBRARY_MANAGER_05', 'A(z) "[y]" library verziója ("[x]") nem támogatott.');
+define('LAN_LIBRARY_MANAGER_06', 'A(z) "[x]" változata a(z) "[y]" library-nak nem található.');
+define('LAN_LIBRARY_MANAGER_07', 'hiányzó függőség');
+define('LAN_LIBRARY_MANAGER_08', 'inkompatibilis függőség');
+define('LAN_LIBRARY_MANAGER_09', 'nem található');
+define('LAN_LIBRARY_MANAGER_10', 'meghatározhatatlan');
+define('LAN_LIBRARY_MANAGER_11', 'nem támogatott');
 
 
 ?>
