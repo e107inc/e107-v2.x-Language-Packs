6f6e55725c18000f548444dae5384633dbe765e9
Gábor <yesszus@gmail.com>
2016-05-18 12:37:39 +0200

    Update lan_lancheck.php

--- Hungarian/e107_languages/Hungarian/admin/lan_lancheck.php
+++ Hungarian/e107_languages/Hungarian/admin/lan_lancheck.php
@@ -1,24 +1,29 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/04/09 16:16:09
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 define("LAN_CHECK_2", "Ellenőrzés indítása");
 define("LAN_CHECK_3", "Ellenőrzés");
-define("LAN_CHECK_4", "Hiányzó file!");
+define("LAN_CHECK_4", "Hiányzó fájl!");
 define("LAN_CHECK_5", "Hiányzó kifejezés!");
 define("LAN_CHECK_15", "Illegális karakterek vagy szóköz a [x] előtt vagy a [y] után");
 define("LAN_CHECK_16", "Eredeti Fájl");
-define("LAN_CHECK_17", "Egy írási probléma keletkezett a file mentése során.");
+define("LAN_CHECK_17", "Egy írási probléma keletkezett a fájl mentése során.");
 define("LAN_CHECK_18", "Nem érhető el nyelvi fájl ehhez a bővítményhez/sminkhez.");
 define("LAN_CHECK_19", "Nem-UTF-8 karakterek!");
 define("LAN_CHECK_20", "Nyelvi csomag generálása");
 define("LAN_CHECK_21", "Ismételt ellenőrzés");
-define("LAN_CHECK_22", "Smink (theme)");
+define("LAN_CHECK_22", "Sablon (theme)");
 define("LAN_CHECK_23", "Hibák");
 define("LAN_CHECK_24", "Összegzés");
-define("LAN_CHECK_25", "Sminkek");
+define("LAN_CHECK_25", "Sablonok");
 define("LAN_CHECK_PAGE_TITLE", "Nyelvek");
 define("LAN_CHECK_27", "Nyelvi csomag hibák száma");
 
@@ -36,3 +41,4 @@ define("LANG_LAN_119", "Ellenőrizd, hogy a CORE_LC és a CORE_LC2 rendelkezik-e
 define("LANG_LAN_120", "Győződj meg róla, hogy az alapértelmezett mappanevek vannak használatban az e107_config.php (eg. e107_languages/, e107_plugins/ etc.) fájlban, majd próbáld újra.");
 
 define("LANG_LAN_AGR", "Megjegyzés: Ennek az eszköznek a használatával beleegyezel, hogy megosztod a nyelvi csomagot az e107 közösséggel.");
+?>
