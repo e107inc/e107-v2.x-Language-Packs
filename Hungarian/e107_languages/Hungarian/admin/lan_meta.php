fbafa5c27e40cd507d3533be058ff0257d921ea0
Gábor <yesszus@gmail.com>
2016-05-18 13:14:59 +0200

    Update lan_meta.php

--- Hungarian/e107_languages/Hungarian/admin/lan_meta.php
+++ Hungarian/e107_languages/Hungarian/admin/lan_meta.php
@@ -1,11 +1,18 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/05/17 21:28:48
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 
 define("METLAN_00", "Meta Tag-ek");
 
 define("METLAN_1", "További meta Tag-ek");
 define("METLAN_2", "Pl.: < meta name='revisit-after' content='30 days' />");
-define("METLAN_3", "Használd a Hírek címét és az összegzést, mint eegy meta-leírás a Hírek oldalon.");
+define("METLAN_3", "Használd a Hírek címét és az összegzést, mint egy meta-leírás a Hírek oldalon.");
+?>
