7bd225f4795b7925bfd07127ab1dd9ba6ed47c7e
tgtje <the_guardian2@hotmail.com>
2016-09-25 15:21:16 +0200

    Update date 20/09/2016

---
+++ Dutch/e107_plugins/log/languages/Dutch_admin.php
@@ -0,0 +1,93 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Dutch Language File.
+|
+|     $URL:$
+|     $Revision:$
+|     $Date:$
+|     $Author: Yvalni, Tgtje $
++----------------------------------------------------------------------------+
+*/
+define("ADSTAT_LAN_1", "Deze plugin logt alle bezoeken aan je site en creëert statistiekoverzichten gebaseerd op de verzamelde gegevens.");
+define("ADSTAT_LAN_2", "De statistiek logger is succesvol geïnstalleerd. Ga aub. naar het configuratie scherm om te activeren. <br /><b>Je dient de permissies/rechten op de e107_plugins/log/logs map te chmodden op 777</b>");
+define("ADSTAT_LAN_3", "Statistiek log");
+define("ADSTAT_LAN_4", "Activeer statistiek loggen");
+define("ADSTAT_LAN_5", "Statistiek Typen");
+define("ADSTAT_LAN_6", "Browsers");
+define("ADSTAT_LAN_7", "Besturings Systemen");
+define("ADSTAT_LAN_8", "Schermresoluties / kleurdieptes");
+define("ADSTAT_LAN_9", "Bezocht door landen/domeinen");
+define("ADSTAT_LAN_10", "Verwezen vanaf");
+define("ADSTAT_LAN_11", "Zoekmachine opvraagteksten");
+define("ADSTAT_LAN_12", "Statistieken  her-instellen");
+define("ADSTAT_LAN_13", "Dit zal de alle tijden statistieken verwijderen - voorzichtig! Verwijderd de statistieken tot aan eind van gisteren. Er is een aparte menu optie beschikbaar om op tijd  geselecteerde data te verwijderen");
+define("ADSTAT_LAN_14", "Pagina Telling");
+define("ADSTAT_LAN_16", "Site statistiek telling");
+define("ADSTAT_LAN_18", "Stel toegang tot hoofd statistiek pagina in voor..");
+define("ADSTAT_LAN_19", "Recente bezoekers");
+define("ADSTAT_LAN_20", "Tel beheerbezoeken");
+define("ADSTAT_LAN_21", "Max aantal te tonen records op statistiekpagina");
+define("ADSTAT_LAN_22", "Start bijwerkfunctie");
+define("ADSTAT_LAN_23", "logs van een voorgaande versie van e107 zijn nog aanwezig, werk ze hier bij");
+define("ADSTAT_LAN_24", "Ga naar het bijwerkscript");
+define("ADSTAT_LAN_26", "Verwijder<br> pagina regels");
+define("ADSTAT_LAN_27", "als je stats onjuiste pagina's bevatten, kun je ze hier verwijderen");
+define("ADSTAT_LAN_28", "Open pagina");
+define("ADSTAT_LAN_29", "Pagina naam");
+define("ADSTAT_LAN_30", "Aankruisen om te verwijderen");
+define("ADSTAT_LAN_31", "Verwijder geselecteerde pagina's");
+define("ADSTAT_LAN_32", "Pagina schonen");
+define("ADSTAT_LAN_35", "Opties");
+define("ADSTAT_LAN_36", "Data Export");
+define("ADSTAT_LAN_38", "De e107_plugins/log/logs/ map moet beschrijfbaar zijn");
+define("ADSTAT_LAN_39", "Statistiek Log Funkties");
+define("ADSTAT_LAN_40", "Exporteer log data");
+define("ADSTAT_LAN_41", "Datum selectie");
+define("ADSTAT_LAN_42", "Enkele dag");
+define("ADSTAT_LAN_43", "Dagelijks voor een maand");
+define("ADSTAT_LAN_44", "Maandelijks voor een jaar");
+define("ADSTAT_LAN_45", "Alle tijden");
+define("ADSTAT_LAN_46", "Datum:");
+define("ADSTAT_LAN_47", "Ongeldige datum gekozen");
+define("ADSTAT_LAN_48", "Maandelijks en alle tijden");
+define("ADSTAT_LAN_49", "Alleen alle tijden");
+define("ADSTAT_LAN_50", "Geen");
+define("ADSTAT_LAN_51", "Uitkomsten data");
+define("ADSTAT_LAN_52", "Pagina data");
+define("ADSTAT_LAN_53", "Geen keuze mogelijk");
+define("ADSTAT_LAN_54", "Ongeldig type keuze");
+define("ADSTAT_LAN_55", "Een apostrof");
+define("ADSTAT_LAN_56", "Aanhalingsteken");
+define("ADSTAT_LAN_57", "Komma");
+define("ADSTAT_LAN_58", "Verticale lijn (|)");
+define("ADSTAT_LAN_59", "CSV scheidingsteken, aanhalingstekens");
+define("ADSTAT_LAN_60", "Haal site adres uit URLs");
+define("ADSTAT_LAN_61", "(wanneer geselecteerd, geeft alleen pagina referentie)");
+define("ADSTAT_LAN_62", "Alle tijden (gedetailleerd)");
+define("ADSTAT_LAN_63", "Beschikbare datasets");
+define("ADSTAT_LAN_64", "Database rijen gevonden");
+define("ADSTAT_LAN_65", "DB filter-tekenreeks");
+define("ADSTAT_LAN_66", "Toon Datasets");
+define("ADSTAT_LAN_67", "Genereer een CSV (komma gescheiden variabele) bestand van historische statistieken welke voldoen aan de specifieke criteria.");
+define("ADSTAT_LAN_68", "Toon de statistieken database reeksen die werkelijk bestaan en aan de selectiecriteria voldoen");
+define("ADSTAT_LAN_69", "Verwijder<br> historische data");
+define("ADSTAT_LAN_70", "Verwijder data ouder dan:");
+define("ADSTAT_LAN_71", "Verwijder data");
+define("ADSTAT_LAN_72", "Bevestig het verwijderen van data ouder dan eerste dag van:");
+define("ADSTAT_LAN_74", "(lijst van data reeksen welke zullen worden verwijdert hieronder)");
+define("ADSTAT_LAN_75", "Reeksen voor verwijdering");
+define("ADSTAT_LAN_76", "Voorzichtig! Eenmaal verwijderd kan de data niet worden hersteld. Backup of exporteer eerst je database");
+define("ADSTAT_LAN_77", "Reeksen verwijderd:");
+define("ADSTAT_LAN_78", "Toon vorige maand, als ook huidige maand voor niet-pagina bezoeken gegevens");
+define("ADSTAT_LAN_79", "Alleen gebruikt indien maand statistieken zijn verzameld");
+define("ADSTAT_LAN_80", "De volgende pagina´s werden verwijderd:");
+define("ADSTAT_LAN_81", "Statistieken geleegd :");
+define("ADSTAT_LAN_82", "Volgende waarden nu ingesteld:");
+define("ADSTAT_LAN_83", "Volgende log ID invoeringen verwijderd");
+define("ADSTAT_LAN_84", "Deze procedure zal de statistiek log samenvattingen overschrijven. (ruwe log bestanden blijven ongewijzigd). Wanneer vervangen, kan de data niet worden hersteld. Maak aub eerst een backup of exporteer de database.");
+define("ADSTAT_LAN_85", "[x] log bestanden werden gevonden. Klik op onderstaande knop om deze bestanden te verwerken.");
+define("ADSTAT_LAN_86", "Totaal hits");
+
+
+?>
