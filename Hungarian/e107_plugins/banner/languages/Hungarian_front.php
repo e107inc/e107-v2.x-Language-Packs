36c6d2c580934fdf92f216b256d1345a88a5463d
Gábor <yesszus@gmail.com>
2016-10-31 17:58:42 +0100

    Updated

--- Hungarian/e107_plugins/banner/languages/Hungarian_front.php
+++ Hungarian/e107_plugins/banner/languages/Hungarian_front.php
@@ -1,8 +1,14 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/05/06 12:21:54
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 
 define("PAGE_NAME", "Banner");
 define("BANNERLAN_16", "Ügyfélnév:");
@@ -17,10 +23,11 @@ define("BANNERLAN_25", "Kattintás %");
 define("BANNERLAN_26", "Megjelenések");
 define("BANNERLAN_27", "Megjelenések maximális száma");
 define("BANNERLAN_28", "Hátralevő megjelenések száma");
-define("BANNERLAN_29", "Nincsennek bannerek");
+define("BANNERLAN_29", "Még nincs banner");
 define("BANNERLAN_30", "Korlátlan");
 define("BANNERLAN_31", "Nem elérhető");
 define("BANNERLAN_34", "Befejezés:");
 define("BANNERLAN_35", "Átkattintások IP-címei");
 define("BANNERLAN_36", "Aktív:");
 define("BANNERLAN_37", "Kezdés:");
+define("BANNERLAN_39", "Nincs kép hozzárendelve a banner-hez.");
