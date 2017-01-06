ac89cfe9e2e087cc0e1e196ce0149500ba520c49
Gábor <yesszus@gmail.com>
2017-01-06 10:22:44 +0100

    Add files via upload

--- Hungarian/e107_plugins/faqs/languages/Hungarian/Hungarian_admin.php
+++ Hungarian/e107_plugins/faqs/languages/Hungarian/Hungarian_admin.php
@@ -1,19 +1,27 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/01/02 07:56:44
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 
 define("LANA_FAQ_QUESTION", "Kérdés");
 define("LANA_FAQ_ANSWER", "Válasz");
+define("LANA_FAQ_UNANSWERED", "Megválaszolatlan");
 define("LANA_FAQ_COMMENT", "Hozzászólás");
 define("LANA_FAQ_UNAME", "Felhasználói név");
-define("LANA_FAQ_ULOGINNAME", "Bejelentkezési név");
+define("LANA_FAQ_ULOGINNAME", "Bejelentkező név");
 define("LANA_FAQ_TAGS", "Címkék");
 define("LANA_FAQ_TAGS_HELP", "Vesszővel elválasztott címke lista");
-define("LANA_FAQ_METAD", "Meta leírás");
+define("LANA_FAQ_METAD", "Meta ismertetés");
 define("LANA_FAQ_METAK", "Meta kulcsszavak");
 
 define("LANA_FAQ_PREF_1", "Beküldés engedélyezése");
-define("LANA_FAQ_PREF_2", "'Kérdés feltevése' korlátozása");
+define("LANA_FAQ_PREF_2", "'Kérdés feltevés' korlátozás");
 define("LANA_FAQ_PREF_3", "Klasszikus elrendezés használata");
+?>
--- Hungarian/e107_plugins/linkwords/languages/Hungarian_admin.php
+++ Hungarian/e107_plugins/linkwords/languages/Hungarian_admin.php
@@ -1,8 +1,14 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/03/19 14:09:08
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 
 define("LWLAN_4", "Nincs link szó definiálva.");
 define("LWLAN_5", "Szavak");
@@ -26,7 +32,6 @@ define("LWLAN_35", "Szövegtörzsek");
 define("LWLAN_36", "Leírások (linkek stb)");
 define("LWLAN_40", "Felhasználó által megadott címek (pl. fórum)");
 define("LWLAN_41", "Felhasználó által megadott szövegtörzsek (pl. fórum)");
-
 define("LWLAN_50", "Buboréksúgó");
 define("LWLAN_51", "Inaktív");
 define("LWLAN_52", "Csak a linkszó");
@@ -41,17 +46,18 @@ define("LWLAN_62", "Buboréksúgó ID (LW ID)");
 define("LWLAN_63", "Csak pozitív egész számok");
 define("LWLAN_64", "Link elhagyása a jelenlegi oldalon");
 define("LWLAN_65", "Ha bejelölöd, elhgyja a kattintható linket, ha az az aktuális oldalra mutat");
-
 define("LAN_LW_HELP_00", "Linkszó Súgó");
-define("LAN_LW_HELP_01", "
-  <b>Engedélyezési területek</b><br>azok a szöveg-területek, ahol a linkszavak engedélyezve lesznek.<br><br>
+define("LAN_LW_HELP_01", "  <b>Engedélyezési területek</b><br>azok a szöveg-területek, ahol a linkszavak engedélyezve lesznek.<br><br>
   <b>Tiltott területek</b><br>azok a területek, ahol a linkszavak használata nem lesz engedélyezve. Ugyanolyan szintaxisban add meg őket, mint a menüknél, soronként egy útvonal-minta.
   Ha a minta '!' jellel végződik, akkor egy konkrét útvonalként lesz értelmezve, máskülönben a szövegrészen belül is lehetnek találatok.<br><br>
   <b>Ajax engedélyezése</b><br>A buboréksúgó tartalma ajax kérésen keresztül kapja az értéket.<br><br>
   <b>Link elhagyása a jelenlegi oldalon</b><br>Azon szavak kiszűrése, melyek az aktuális oldalhoz vannak rendelve, tehát azon linkek nem lesznek megjelenítve, melyek önmagukra mutatnak.<br>");
-define("LAN_LW_HELP_02", "Azon szavak meghatározása, melyek kattintható linkekké lesznek alakítva, vagy amikre buboréksúgó lesz helyezve<br><br>
-  <b><u>Szavak linkekké alakítása</u></b><br>A megadásnál ügyelj arra, hogy a kis- és nagybetű nem azonos. Több szó esetén vesszővel elválasztva add meg őket.<br><br>
-  <b><u>Link</u></b><br>Kattintható linkek megadása. Ha külső hivatkozást adsz meg, a hivatkozásnak http://-rel kell kezdődnie. Ha belső hivatkozást szeretnél, úgy a {e_XXX} konstansok használhatóak.<br><br>
-  <b><u>Buboréksúgó</u></b><br>Egy buboréksúgó jelenik meg a megadott szó fölött, ha az egérmutató a szó fölé kerül.<br><br>
-  <b><u>LW ID (Buboréksúgó ID)</u></b><br><br>Optimális, numerikus ID meghatározása az Ajax-kérésekhez. Üresen hagyva az adatbázisban található ID lesz használva.<br><br>
-  <b><u>Aktivál?</u></b><br>Meghatározható, mely beállítások legyenek aktívak.");
+define("LAN_LW_HELP_02", "Azon szavak meghatározása, melyek kattintható linkekké lesznek alakítva, vagy amikre buboréksúgó lesz helyezve<br /><br />
+  <b><u>Szavak linkekké alakítása</u></b><br />A megadásnál ügyelj arra, hogy a kis- és nagybetű nem azonos. Több szó esetén vesszővel elválasztva add meg őket.<br /><br />
+  <b><u>Link</u></b><br />Kattintható linkek megadása. Ha külső hivatkozást adsz meg, a hivatkozásnak \"http://\"-vel [/i]kell[/i] kezdődnie. Ha belső hivatkozást szeretnél, úgy a {e_XXX} konstansok használhatóak.<br /><br />
+  <b><u>Buboréksúgó</u></b><br />Egy buboréksúgó jelenik meg a megadott szó fölött, ha az egérmutató a szó fölé kerül.<br /><br />
+  <b><u>LW ID (Buboréksúgó ID)</u></b><br /><br />Optimális, numerikus ID meghatározása az Ajax-kérésekhez. Üresen hagyva az adatbázisban található ID lesz használva.<br /><br />
+  <b><u>Aktivál?</u></b><br />Meghatározható, mely beállítások legyenek aktívak.");
+
+
+?>
