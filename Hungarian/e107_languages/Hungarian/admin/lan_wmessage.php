c1730c76a388fbfe05c8a3d375a389f528a8b408
Gábor <yesszus@gmail.com>
2017-01-10 12:07:02 +0100

    Add files via upload

--- Hungarian/e107_languages/Hungarian/admin/lan_wmessage.php
+++ Hungarian/e107_languages/Hungarian/admin/lan_wmessage.php
@@ -1,16 +1,24 @@
 <?php
-
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/01/07 13:08:21
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
 
 define("WMLAN_00","Üdvözlő Üzenet");
 define("WMLAN_02","Üzenet");
 define("WMLAN_04","Üzenet szövege");
-
 define("WMLAN_05","Keret");
 define("WMLAN_06","Jelöld be, ha szeretnéd, hogy az üzenet a weboldal sablonjának stílusában jelenjen meg");
 define("WMLAN_07","Az alaprendszer felülírása a {WMESSAGE} shortcode használatához:");
-
 define("WMLAN_09","Nincsenek üdvözlő üzenetek");
 define("WMLAN_10","Üzenet Fejléc");
+define("WMLAN_11","Carousel körbezárás");
+define("WMLAN_12","Üdvözlő Üzenet Sugó");
+define("WMLAN_13","Itt kezelhetjük a weboldalon megjelenő üdvözlő üzeneteket, amelyeket aktiválhatunk és kikapcsolhatunk.<br/>Üzeneteket jeleníthet meg a vendégek, regisztrált / bejelentkezett tagok, moderátorok és az adminok számára.");
+?>
